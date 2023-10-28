f3b7faf8751a1d9999dd2a0ea2ee00494cba22a1
Parthik04 <144251798+Parthik04@users.noreply.github.com>
2023-10-28 17:31:46 -0300

    portfolio

---
+++ app.js
@@ -0,0 +1,18 @@
+// author: Parthik 
+// date: 28 October 2023
+// 
+// function for active button toggle darkmode and light mode---
+// reference by w3school and youtube tutorial
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
